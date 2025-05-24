f24fe643dabd772769232609e08b7db72dffd5bd
Marcos Calavia <mcalaviaf@icloud.com>
2025-05-24 19:44:19 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,283 @@
+info.onCountdownEnd(function () {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+let tourist: Sprite = null
+let babyDino: Sprite = null
+scene.setBackgroundImage(sprites.background.cityscape2)
+let mamaDino = sprites.create(assets.image`Mama`, SpriteKind.Player)
+info.startCountdown(15)
+controller.moveOnlyOnscreenWithArrows(mamaDino, controller.Speeds.Fast)
+scroller.scrollBackgroundWithSpeed(-50, 0)
+info.startCountdown(15)
+animation.runImageAnimation(
+mamaDino,
+[img`
+    ........................
+    ........................
+    ...........cc...........
+    ...........cccc.........
+    .......cc...ccccccc.....
+    .......cccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb555555ff155555c
+    .....ccb55555555ff55d55c
+    ......b5555555555555555c
+    ...c..b555d55555bb13bbc.
+    ...cccd55ddddd55bb3335c.
+    ....cbdddddddddd55b335c.
+    ..cccdddddb55bdddd5555c.
+    ..cccdddddb555bbbbcccc..
+    ...ccddddddb5555cbcdc...
+    ccccbdddddddcb55cbcc....
+    cddddddddd55dbccbbc.....
+    cbdddddddd555dbbbcc.....
+    .ccbdddbbdd555bbcdbcc...
+    ...cccbbbbdd55ccdddbc...
+    ......cccbdddbccccccc...
+    ........cdd555dc........
+    `,img`
+    ........................
+    ........................
+    ...........ccc..........
+    ...........cccc.........
+    .......ccc..ccccccc.....
+    .......cccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb555555ff155555c
+    ......cb55555555ff55d55c
+    ......b5555555555555555c
+    ...cc.b555dd5555bb13bbc.
+    ...cccd55ddddd555b3335c.
+    .....bdddddddddd55b335c.
+    ..cccdddddb55bbddd5555c.
+    ..cccdddddb555bbbbcccc..
+    ...ccddddddb5555cbcdc...
+    ccccbdddddd5cb55cbcc....
+    cddddddddd5555ccbbc.....
+    .cddddddbdd555bbbcc.....
+    ..ccdddbbbdd55cbcdc.....
+    ....ccbbcbddddccdddcc...
+    ......cccdd555dcccccc...
+    ........cccccccc........
+    `,img`
+    ........................
+    ............cc..........
+    ............ccc.........
+    ........ccc.ccccccc.....
+    ........ccccc555555cc...
+    ........ccb5555555555c..
+    .....ccc.b55555ff15555c.
+    .....cccb5555555ff55555c
+    ......cb555555555555d55c
+    ....c.b555555555bb55555c
+    ....ccb555ddd5555b13bbc.
+    ....ccd55ddddd555b3335c.
+    .....cdd5ddddddd55b335c.
+    ...c.bddddb555bbbd555c..
+    ...ccdddddbb55555bccc...
+    ...ccdddddddcc555bcc....
+    ...ccddddddddbcccbcccc..
+    .ccbddddddd55dbbbbc55c..
+    ccddddddddd555dbbcc5c...
+    cddddddbbbdd555bbccc....
+    .ccddddbbbbdd55bcc......
+    ...cccbbbbbdddbcddcc....
+    .....cccccdd555dcccc....
+    ..........cccccc........
+    `,img`
+    ........................
+    ............cc..........
+    ............ccc.........
+    ........ccc.ccccccc.....
+    ........ccccc555555cc...
+    ........ccb5555555555c..
+    .....ccc.b55555ff15555c.
+    .....cccb5555555ff55555c
+    ......cb555555555555d55c
+    ....c.b555555555bb55555c
+    ....ccb555ddd5555b13bbc.
+    ....ccd55ddddd555b3335c.
+    .....cdd5ddddddd55b335c.
+    ...c.bddddb555bbbd555c..
+    ...ccdddddbb55555bccc...
+    ...ccdddddddcc555bcc....
+    .ccccdddddddddcccbcccc..
+    .cdcdddddddd55dbbbc55c..
+    .cdddddddddd555dccc5c...
+    .cbddddbbbbdd5d555cc....
+    ..cbdddbbbbbdd5555......
+    ...cccbbbbbbd5555c......
+    .....cccccccc555c.......
+    .............ccc........
+    `,img`
+    ........................
+    ............cc..........
+    ............ccc.........
+    ........ccc.ccccccc.....
+    ........ccccc555555cc...
+    ........ccb5555555555c..
+    .....ccc.b55555ff15555c.
+    .....cccb5555555ff55555c
+    ......cb555555555555d55c
+    ....c.b555555555bb55555c
+    ....ccb555ddd5555b13bbc.
+    ....ccd55ddddd555b3335c.
+    .....cdd5ddddddd55b335c.
+    ...c.bddddb555bbbd555c..
+    ...ccdddddb555555bccc...
+    ..cccddddddcc5555bcc....
+    .cdccddddddddbcccbcccc..
+    .cddbdddddddddbbbbc55c..
+    .cdddddddddd55dbbbc5c...
+    .cbddddbbbbd55ddbccc....
+    ..cbdddbbbbd555dccc.....
+    ...cccbbbbbbddd555c.....
+    .....ccccccbd55555c.....
+    ...........cc5555c......
+    `,img`
+    ........................
+    ............cc..........
+    ............ccc.........
+    ........cc..ccccccc.....
+    ........ccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb555555ff155555c
+    ......cb55555555ff55d55c
+    ......b5555555555555555c
+    ...cc.b555dd5555bb13bbc.
+    ...cccd55ddddd555b3335c.
+    ....ccdd5ddddddd55b335c.
+    .....bddddb55bdddd5555c.
+    ..cccdddddb55bbbbbcccc..
+    .ccccddddddb5555cbcccc..
+    .cdccdddddddc555cbc55c..
+    .cdddddddddddcccbbc5c...
+    .cbddddddd55dbbbbccc....
+    .ccbdddddd555dbbbcbc....
+    ..cccddbbbd555bbccc.....
+    ....ccbbbbbd555cc.......
+    ......ccccbddddbc.......
+    ..........cd5555dc......
+    `],
+100,
+true
+)
+forever(function () {
+    babyDino = sprites.createProjectileFromSide(assets.image`Baby`, -90, 0)
+    babyDino.y = randint(15, 115)
+    pause(1000)
+    animation.runImageAnimation(
+    babyDino,
+    assets.animation`Animated Baby`,
+    1000,
+    true
+    )
+})
+forever(function () {
+    tourist = sprites.createProjectileFromSide(img`
+        . . . . . . . 8 8 8 8 8 . . . . 
+        . . . . . 8 8 6 6 6 6 6 8 . . . 
+        . . . . 8 8 6 6 6 6 6 6 6 8 . . 
+        . . . . 8 9 7 6 6 6 7 7 7 b 8 . 
+        . . 8 8 9 9 7 6 6 6 7 7 7 9 b 8 
+        . 8 6 6 9 9 7 7 6 6 6 7 7 9 9 8 
+        8 6 6 6 9 9 6 7 7 7 7 7 6 9 9 8 
+        8 6 6 6 9 9 8 8 8 8 8 8 8 9 9 8 
+        8 6 6 6 9 b 8 b b b 8 b 8 b 9 8 
+        8 6 8 8 8 8 b b b b 8 b b 8 b 8 
+        8 8 3 3 8 8 6 6 6 6 8 6 6 8 8 8 
+        8 3 3 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 f f f 8 8 8 8 f f f 8 8 
+        . 8 8 f c b b f 8 8 f c b b f . 
+        . . . . f f f . . . . f f f . . 
+        `, -90, 0)
+    tourist.setKind(SpriteKind.Enemy)
+    tourist.y = randint(15, 115)
+    pause(2100)
+    animation.runImageAnimation(
+    tourist,
+    [img`
+        . . . . . . . 8 8 8 8 8 . . . . 
+        . . . . . 8 8 6 6 6 6 6 8 . . . 
+        . . . . 8 8 6 6 6 6 6 6 6 8 . . 
+        . . . . 8 9 7 6 6 6 6 6 7 b 8 . 
+        . . 8 8 9 9 7 7 6 6 6 6 7 9 b 8 
+        . 8 6 6 9 9 7 7 7 6 6 6 7 9 9 8 
+        8 6 6 6 9 9 6 7 7 7 7 7 6 9 9 8 
+        8 6 6 6 9 9 8 8 8 8 8 8 8 9 9 8 
+        8 6 6 6 9 b 8 b b b 8 b 8 b 9 8 
+        8 6 8 8 8 8 b b b b 8 b b 8 b 8 
+        8 8 3 3 8 8 6 6 6 6 8 6 6 8 8 8 
+        8 3 3 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 f f f 8 8 8 8 f f f 8 8 
+        . 8 8 f b c c f 8 8 f b c c f . 
+        . . . . b b f . . . . b b f . . 
+        `,img`
+        . . . . . . . 8 8 8 8 8 . . . . 
+        . . . . . 8 8 6 6 6 6 6 8 . . . 
+        . . . . 8 8 6 6 6 6 6 6 6 8 . . 
+        . . . . 8 9 7 7 7 6 6 6 7 b 8 . 
+        . . 8 8 9 9 7 7 7 7 6 6 7 9 b 8 
+        . 8 6 6 9 9 7 7 7 7 7 6 7 9 9 8 
+        8 6 6 6 9 9 6 7 7 7 7 7 6 9 9 8 
+        8 6 6 6 9 9 8 8 8 8 8 8 8 9 9 8 
+        8 6 6 6 9 b 8 b b b 8 b 8 b 9 8 
+        8 6 8 8 8 8 b b b b 8 b b 8 b 8 
+        8 8 3 3 8 8 6 6 6 6 8 6 6 8 8 8 
+        8 3 3 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 f f f 8 8 8 8 f f f 8 8 
+        . 8 8 f f f b f 8 8 f f f b f . 
+        . . . . c b b . . . . c b b . . 
+        `,img`
+        . . . . . . . 8 8 8 8 8 . . . . 
+        . . . . . 8 8 6 6 6 6 6 8 . . . 
+        . . . . 8 8 6 6 6 6 6 6 6 8 . . 
+        . . . . 8 9 7 6 6 6 7 7 7 b 8 . 
+        . . 8 8 9 9 7 6 6 6 7 7 7 9 b 8 
+        . 8 6 6 9 9 7 7 6 6 6 7 7 9 9 8 
+        8 6 6 6 9 9 6 7 7 7 7 7 6 9 9 8 
+        8 6 6 6 9 9 8 8 8 8 8 8 8 9 9 8 
+        8 6 6 6 9 b 8 b b b 8 b 8 b 9 8 
+        8 6 8 8 8 8 b b b b 8 b b 8 b 8 
+        8 8 3 3 8 8 6 6 6 6 8 6 6 8 8 8 
+        8 3 3 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 f f f 8 8 8 8 f f f 8 8 
+        . 8 8 f c b b f 8 8 f c b b f . 
+        . . . . f f f . . . . f f f . . 
+        `,img`
+        . . . . . . . 8 8 8 8 8 . . . . 
+        . . . . . 8 8 6 6 6 6 6 8 . . . 
+        . . . . 8 8 6 6 6 6 6 6 6 8 . . 
+        . . . . 8 9 7 6 6 6 6 6 7 b 8 . 
+        . . 8 8 9 9 7 6 6 6 6 6 7 9 b 8 
+        . 8 6 6 9 9 7 7 6 6 6 6 7 9 9 8 
+        8 6 6 6 9 9 6 7 7 7 7 7 6 9 9 8 
+        8 6 6 6 9 9 8 8 8 8 8 8 8 9 9 8 
+        8 6 6 6 9 b 8 b b b 8 b 8 b 9 8 
+        8 6 8 8 8 8 b b b b 8 b b 8 b 8 
+        8 8 3 3 8 8 6 6 6 6 8 6 6 8 8 8 
+        8 3 3 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 f f f 8 8 8 8 f f f 8 8 
+        . 8 8 f b b c f 8 8 f b b c f . 
+        . . . . c f f . . . . c f f . . 
+        `],
+    2100,
+    true
+    )
+})
